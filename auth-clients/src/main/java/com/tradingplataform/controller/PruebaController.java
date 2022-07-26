b9f9f0ed120e9ea32e0e0cd7c68849c143966a19
VASS\mgomezgarrote <mgomezgarrote@serbatic.es>
2022-07-26 09:47:38 +0200

    fin configuración corto circuito

--- auth-clients/src/main/java/com/tradingplataform/controller/PruebaController.java
+++ auth-clients/src/main/java/com/tradingplataform/controller/PruebaController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,8 @@ import com.tradingplataform.models.User;
 import com.tradingplataform.models.resttemplates_feign.Product;
 import com.tradingplataform.service.impl.UserService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping("/prueba")
 @CrossOrigin
@@ -27,6 +30,7 @@ public class PruebaController {
 	private UserService userService;
 	
 	// Usando RestTemplate
+	@CircuitBreaker(name = "productCB", fallbackMethod = "fallbackgetProducts")
 	@GetMapping("/product/{userId}")
 	public ResponseEntity<List<Product>> getProducts(@PathVariable("userId") int userId){
 		Optional<User> user = userService.find(userId);
@@ -38,9 +42,14 @@ public class PruebaController {
 			return ResponseEntity.ok(products);
 		}
 	}
+	
+	public ResponseEntity<?> fallbackgetProducts(@PathVariable("userId") int userId, RuntimeException e){
+		return new ResponseEntity<>("Usuario con id: " + userId + " error al buscar sus productos", HttpStatus.OK);
+	}
 	/////////////////////////////////////////////////////////////////
 	
 	// Usando Feign
+	@CircuitBreaker(name = "productCB", fallbackMethod = "fallbackcreateProduct")
 	@PostMapping("/createproduct/{userId}")
 	public ResponseEntity<Product> createProduct(@PathVariable("userId") int userId, @RequestBody Product product){
 		
@@ -53,7 +62,12 @@ public class PruebaController {
 	
 	}
 	
+	public ResponseEntity<?> fallbackcreateProduct(@PathVariable("userId") int userId, @RequestBody Product product, RuntimeException e){
+		return new ResponseEntity<>("Usuario con id: " + userId + " error al crear su producto", HttpStatus.OK);
+	}
+	
 	// Usando Feign
+	@CircuitBreaker(name = "productCB", fallbackMethod = "fallbackgetProductsWithUser")
 	@GetMapping("/getProductsWithUser/{userId}")
 	public ResponseEntity<Map<String, Object>> getProductsWithUser(@PathVariable("userId") int userId){
 		
@@ -61,6 +75,10 @@ public class PruebaController {
 		System.out.println(mapProducts.toString());
 		return ResponseEntity.ok(mapProducts);
 	
+	}
+	
+	public ResponseEntity<?> fallbackgetProductsWithUser(@PathVariable("userId") int userId, RuntimeException e){
+		return new ResponseEntity<>("Usuario con id: " + userId + " error al crear su producto", HttpStatus.OK);
 	}
 	
 	/////////////////////////////////////////////////////////////////
