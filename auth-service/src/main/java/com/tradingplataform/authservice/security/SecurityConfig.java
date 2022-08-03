e616c9c961939dee656f4c441bb6585e94af152e
VASS\mgomezgarrote <mgomezgarrote@serbatic.es>
2022-08-03 11:19:36 +0200

    cors v2

--- auth-service/src/main/java/com/tradingplataform/authservice/security/SecurityConfig.java
+++ auth-service/src/main/java/com/tradingplataform/authservice/security/SecurityConfig.java
@@ -37,7 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.cors().and()
 			.csrf().disable()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
